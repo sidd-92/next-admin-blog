1f8d5c12d44c5aea55baf77ed8fb1fb8efc4feec
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-10-07 18:29:34 +0530

    Basic Home Page Setup

--- components/Layout.js
+++ components/Layout.js
@@ -5,48 +5,22 @@ class Layout extends React.Component {
   render() {
     return (
       <div>
-        <header className="text-gray-700 body-font">
+        <header className="text-gray-700 body-font bg-gray-100">
           <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            {console.log(this.props.router.asPath)}
             <img src="/eat.svg" className="w-12" />
-            <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <span className="hidden lg:flex lg:flex-grow" />
+            <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-2xl justify-center">
               <Link href="/">
-                <a className={`mr-5 hover:text-gray-900 ${this.props.router.asPath === "/" ? "border-b border-purple-500" : ""}`}>Posts</a>
+                <a className={`mr-5 hover:text-gray-900 ${this.props.router.asPath === "/" ? "border-b-2 border-purple-500 text-purple-700" : ""}`}>Posts</a>
               </Link>
               <a className="mr-5 hover:text-gray-900">Second Link</a>
               <a className="mr-5 hover:text-gray-900">Third Link</a>
               <a className="mr-5 hover:text-gray-900">Fourth Link</a>
             </nav>
-            <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
-              Button
-              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </button>
           </div>
         </header>
-        <main className="container mx-auto min-h-screen">{this.props.children}</main>
-        <section className="antialiased font-sans w-full bg-gray-300 text-left text-black py-8">
-          <div className="container mx-auto max-w-xl py-8 font-normal leading-normal">
-            <h3 className="text-2xl">Grow Your Business</h3>
-            <p className="">
-              Build high performing teams, establish design practices, mentor and grow the next generation of great designers, and design interfaces and
-              experiences.
-            </p>
-            <div className="max-w-sm mt-4 sm:flex">
-              <input
-                type="email"
-                className="block w-full focus:outline-0 bg-white py-3 px-6 mb-2 sm:mb-0"
-                name="email"
-                placeholder="Enter your email"
-                required=""
-              />
-              <button className="uppercase text-sm text-white focus:outline-0 w-full sm:w-auto bg-black hover:bg-gray-900 focus:bg-gray-300 tracking-wide px-6">
-                Subscribe
-              </button>
-            </div>
-          </div>
-        </section>
+        <main className="container mx-auto min-h-screen lg:mt-8 lg:mb-10">{this.props.children}</main>
+        <section className="antialiased font-sans w-full bg-gray-300 text-left text-black py-8"></section>
       </div>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -15,32 +15,45 @@ export default class extends Component {
     return (
       <React.Fragment>
         <Head>
-          <title>Create Next App</title>
+          <title>Food Blog Demo</title>
           <link rel="icon" href="/eat.svg" />
         </Head>
         <Layout>
           <section className="text-gray-700 body-font">
-            <div className="container px-5 py-24 mx-auto">
-              <div className="flex flex-wrap w-full mb-20">
-                <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-                  <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Test Food Blog</h1>
-                  <div className="h-1 w-20 bg-indigo-500 rounded"></div>
-                </div>
-                <p className="lg:w-1/2 w-full leading-relaxed text-base">All the below data is coming from API</p>
+            <div className="container px-5 py-4 mx-auto">
+              <div class="flex flex-col text-center w-full mb-20">
+                <h2 class="text-xs lg:text-sm text-indigo-500 tracking-widest font-medium title-font mb-1">Lorem Ipsum</h2>
+                <div class="sm:text-5xl text-2xl font-medium title-font mb-4 text-gray-900">Food Blog Demo</div>
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-base lg:text-2xl text-justify">
+                  Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard
+                  of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.
+                </p>
               </div>
+              <div className="flex flex-col lg:flex-row items-center mb-20">
+                <input
+                  class="w-full lg:w-10/12 bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
+                  placeholder="Search"
+                  type="text"
+                />
+                <button class="w-full lg:w-2/12 text-white bg-indigo-500 mt-1 lg:mt-0 lg:ml-1 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Search
+                </button>
+              </div>
+              <div className="font-bold text-4xl mb-8">Latest Articles</div>
               <div className="flex flex-wrap -m-4">
                 {this.props.posts.map((item, index) => (
-                  <div key={index} className="xl:w-1/4 md:w-1/2 p-4">
-                    <div className="bg-gray-100 p-6 rounded-lg">
-                      <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-                      <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{item.title}</h3>
-                      <h2 className="text-lg text-gray-900 font-medium title-font mb-4">{item.title}</h2>
-                      <p className="leading-relaxed text-base mb-6">{item.content}</p>
-                      <Link href={`/posts/${index}`}>
-                        <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More</a>
-                      </Link>
+                  <Link href={`/posts/${index}`}>
+                    <div key={index} className="lg:w-1/2 p-4 cursor-pointer">
+                      <div className="bg-gray-100 p-6 rounded-lg transition-all duration-150 hover:shadow-2xl">
+                        <img className="h-64 lg:h-72 rounded w-full object-cover object-center mb-6" src="/subji.jpeg" alt="content" />
+                        <div className="tracking-widest text-indigo-500 text-xs font-medium title-font">{item.title}</div>
+                        <div className="text-2xl text-gray-900 font-bold title-font mb-4">{item.title}</div>
+                        <p className="leading-relaxed text-lg mb-6">{item.content}</p>
+
+                        <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">Learn More</a>
+                      </div>
                     </div>
-                  </div>
+                  </Link>
                 ))}
               </div>
             </div>
