d6a2e8c6bc98de03659fe9ad1764582d3430a316
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-10-07 17:15:09 +0530

    Dynamic Roting

--- components/Layout.js
+++ components/Layout.js
@@ -1,46 +1,55 @@
 import Link from "next/link";
-const Layout = (props) => (
-  <div>
-    <header class="text-gray-700 body-font">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <img src="/eat.svg" className="w-12" />
-        <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-gray-900">First Link</a>
-          <a class="mr-5 hover:text-gray-900">Second Link</a>
-          <a class="mr-5 hover:text-gray-900">Third Link</a>
-          <a class="mr-5 hover:text-gray-900">Fourth Link</a>
-        </nav>
-        <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
-          Button
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
+import React from "react";
+import { withRouter } from "next/router";
+class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="text-gray-700 body-font">
+          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            {console.log(this.props.router.asPath)}
+            <img src="/eat.svg" className="w-12" />
+            <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+              <Link href="/">
+                <a className={`mr-5 hover:text-gray-900 ${this.props.router.asPath === "/" ? "border-b border-purple-500" : ""}`}>Posts</a>
+              </Link>
+              <a className="mr-5 hover:text-gray-900">Second Link</a>
+              <a className="mr-5 hover:text-gray-900">Third Link</a>
+              <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+            </nav>
+            <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
+              Button
+              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
+          </div>
+        </header>
+        <main className="container mx-auto min-h-screen">{this.props.children}</main>
+        <section className="antialiased font-sans w-full bg-gray-300 text-left text-black py-8">
+          <div className="container mx-auto max-w-xl py-8 font-normal leading-normal">
+            <h3 className="text-2xl">Grow Your Business</h3>
+            <p className="">
+              Build high performing teams, establish design practices, mentor and grow the next generation of great designers, and design interfaces and
+              experiences.
+            </p>
+            <div className="max-w-sm mt-4 sm:flex">
+              <input
+                type="email"
+                className="block w-full focus:outline-0 bg-white py-3 px-6 mb-2 sm:mb-0"
+                name="email"
+                placeholder="Enter your email"
+                required=""
+              />
+              <button className="uppercase text-sm text-white focus:outline-0 w-full sm:w-auto bg-black hover:bg-gray-900 focus:bg-gray-300 tracking-wide px-6">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </section>
       </div>
-    </header>
-    <main className="container mx-auto min-h-screen">{props.children}</main>
-    <section className="antialiased font-sans w-full bg-gray-300 text-left text-black py-8">
-      <div className="container mx-auto max-w-xl py-8 font-normal leading-normal">
-        <h3 className="text-2xl">Grow Your Business</h3>
-        <p className="">
-          Build high performing teams, establish design practices, mentor and grow the next generation of great designers, and design interfaces and
-          experiences.
-        </p>
-        <div className="max-w-sm mt-4 sm:flex">
-          <input
-            type="email"
-            className="block w-full focus:outline-0 bg-white py-3 px-6 mb-2 sm:mb-0"
-            name="email"
-            placeholder="Enter your email"
-            required=""
-          />
-          <button className="uppercase text-sm text-white focus:outline-0 w-full sm:w-auto bg-black hover:bg-gray-900 focus:bg-gray-300 tracking-wide px-6">
-            Subscribe
-          </button>
-        </div>
-      </div>
-    </section>
-  </div>
-);
+    );
+  }
+}
 
-export default Layout;
+export default withRouter(Layout);
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 import Head from "next/head";
 
@@ -18,23 +19,26 @@ export default class extends Component {
           <link rel="icon" href="/eat.svg" />
         </Head>
         <Layout>
-          <section class="text-gray-700 body-font">
-            <div class="container px-5 py-24 mx-auto">
-              <div class="flex flex-wrap w-full mb-20">
-                <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-                  <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Test Food Blog</h1>
-                  <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+          <section className="text-gray-700 body-font">
+            <div className="container px-5 py-24 mx-auto">
+              <div className="flex flex-wrap w-full mb-20">
+                <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+                  <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Test Food Blog</h1>
+                  <div className="h-1 w-20 bg-indigo-500 rounded"></div>
                 </div>
-                <p class="lg:w-1/2 w-full leading-relaxed text-base">All the below data is coming from API</p>
+                <p className="lg:w-1/2 w-full leading-relaxed text-base">All the below data is coming from API</p>
               </div>
-              <div class="flex flex-wrap -m-4">
+              <div className="flex flex-wrap -m-4">
                 {this.props.posts.map((item, index) => (
-                  <div key={index} class="xl:w-1/4 md:w-1/2 p-4">
-                    <div class="bg-gray-100 p-6 rounded-lg">
-                      <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-                      <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{item.title}</h3>
-                      <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{item.title}</h2>
-                      <p class="leading-relaxed text-base">{item.content}</p>
+                  <div key={index} className="xl:w-1/4 md:w-1/2 p-4">
+                    <div className="bg-gray-100 p-6 rounded-lg">
+                      <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
+                      <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{item.title}</h3>
+                      <h2 className="text-lg text-gray-900 font-medium title-font mb-4">{item.title}</h2>
+                      <p className="leading-relaxed text-base mb-6">{item.content}</p>
+                      <Link href={`/posts/${index}`}>
+                        <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More</a>
+                      </Link>
                     </div>
                   </div>
                 ))}
