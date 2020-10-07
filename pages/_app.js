9247b49f83df6e350db0d883484c2be4b0027499
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-10-07 14:54:17 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import App from "next/app";
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+// Styles
+import "../src/assets/main.css";
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, store, router } = this.props;
+    return (
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    );
+  }
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import fetch from "isomorphic-unfetch";
+export default class extends Component {
+  static async getInitialProps() {
+    const res = await fetch("http://localhost:9000/posts/all");
+    const data = await res.json();
+    return { posts: data };
+  }
+  render() {
+    return (
+      <div>
+        <div>Your Next App is Working</div>
+        {this.props.posts.map((item, index) => (
+          <div style={{ backgroundColor: "#ccc", padding: "10px", width: "180px", marginBottom: "5px" }} key={index}>
+            <div style={{ fontWeight: "bold" }}>{item.title}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
